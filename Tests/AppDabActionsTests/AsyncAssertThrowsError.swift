b64520e569e1d93d1b2c32c2dacca6966909c680
Morten Bjerg Gregersen <morten@mogee.dk>
2021-08-25 20:48:45 +0200

    Create AsyncAssertThrowsError.swift

---
+++ Tests/AppDabActionsTests/AsyncAssertThrowsError.swift
@@ -0,0 +1,34 @@
+import XCTest
+
+extension XCTest {
+    func XCTAssertAsyncThrowsError(
+        _ expression: @autoclosure () async throws -> Void,
+        _ message: @autoclosure () -> String = "",
+        file: StaticString = #filePath,
+        line: UInt = #line,
+        _ errorHandler: (_ error: Error) -> Void = { _ in }
+    ) async {
+        do {
+            _ = try await expression()
+            XCTFail(message(), file: file, line: line)
+        } catch {
+            errorHandler(error)
+        }
+    }
+
+    func XCTAssertAsyncThrowsError<T: Sendable>(
+        _ expression: @autoclosure () async throws -> T,
+        _ message: @autoclosure () -> String = "",
+        file: StaticString = #filePath,
+        line: UInt = #line,
+        _ errorHandler: (_ error: Error) -> Void = { _ in }
+    ) async {
+        do {
+            _ = try await expression()
+            XCTFail(message(), file: file, line: line)
+        } catch {
+            errorHandler(error)
+        }
+    }
+
+}
