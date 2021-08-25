ecef0b45caa9c3749797be972937302da96bea38
Morten Bjerg Gregersen <morten@mogee.dk>
2021-08-25 21:00:09 +0200

    Update RegisterDevice

--- Sources/AppDabActions/Actions/Online/Devices/RegisterDevice.swift
+++ Sources/AppDabActions/Actions/Online/Devices/RegisterDevice.swift
@@ -1,8 +1,8 @@
 import Bagbutik
 
-public func registerDevice(named name: String, platform: BundleIdPlatform, udid: String) throws {
+public func registerDevice(named name: String, platform: BundleIdPlatform, udid: String) async throws {
     let requestBody = DeviceCreateRequest(data: .init(attributes: .init(name: name, platform: platform, udid: udid)))
     ActionsEnvironment.logger.info("🚀 Registering a new device called '\(name)' (\(udid)) for \(platform.prettyName)...")
-    _ = try ActionsEnvironment.service.requestSynchronously(.createDevice(requestBody: requestBody)).get()
+    _ = try await ActionsEnvironment.service.request(.createDevice(requestBody: requestBody))
     ActionsEnvironment.logger.info("👍 Device registered")
 }
