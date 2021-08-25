86de9e9596c1b8fa81ee3b7b39bcb4eec6575769
Morten Bjerg Gregersen <morten@mogee.dk>
2021-08-25 21:07:27 +0200

    Update InviteUser

--- Sources/AppDabActions/Actions/Online/Users/InviteUser.swift
+++ Sources/AppDabActions/Actions/Online/Users/InviteUser.swift
@@ -1,6 +1,6 @@
 import Bagbutik
 
-public func inviteUser(email: String, firstName: String, lastName: String, roles: [UserRole], allAppsVisible: Bool = false, provisioningAllowed: Bool = false, visibleAppIds: [String]? = nil) throws {
+public func inviteUser(email: String, firstName: String, lastName: String, roles: [UserRole], allAppsVisible: Bool = false, provisioningAllowed: Bool = false, visibleAppIds: [String]? = nil) async throws {
     var relationships: UserInvitationCreateRequest.Data.Relationships? = nil
     if let visibleAppsData = visibleAppIds?.map({ UserInvitationCreateRequest.Data.Relationships.VisibleApps.Data(id: $0) }) {
         relationships = .init(visibleApps: .init(data: visibleAppsData))
@@ -9,6 +9,6 @@ public func inviteUser(email: String, firstName: String, lastName: String, roles
         attributes: .init(allAppsVisible: allAppsVisible, email: email, firstName: firstName, lastName: lastName, provisioningAllowed: provisioningAllowed, roles: roles),
         relationships: relationships))
     ActionsEnvironment.logger.info("🚀 Inviting user '\(firstName) \(lastName)' (\(email))...")
-    _ = try ActionsEnvironment.service.requestSynchronously(.createUserInvitation(requestBody: requestBody)).get()
+    _ = try await ActionsEnvironment.service.request(.createUserInvitation(requestBody: requestBody))
     ActionsEnvironment.logger.info("👍 User invited")
 }
