32d22a990f8fe00bdc9ecc35980db32233255368
Morten Bjerg Gregersen <morten@mogee.dk>
2021-08-31 21:33:15 +0200

    Return created and updated user invitations

---
+++ Sources/AppDabActions/Actions/Online/Users/ListUserInvitations.swift
@@ -0,0 +1,12 @@
+import Bagbutik
+
+@discardableResult
+public func listUserInvitations() async throws -> [UserInvitation] {
+    ActionsEnvironment.logger.info("🚀 Fetching list of user invitations...")
+    let response = try await ActionsEnvironment.service.request(.listUserInvitations())
+    ActionsEnvironment.logger.info("👍 User invitations fetched")
+    response.data.map(\.attributes).forEach { userAttributes in
+        ActionsEnvironment.logger.info(" ◦ \(userAttributes!.firstName!) \(userAttributes!.lastName!) (\(userAttributes!.email!))")
+    }
+    return response.data
+}
---
+++ Tests/AppDabActionsTests/Actions/Online/Users/ListUserInvitationsTests.swift
@@ -0,0 +1,29 @@
+import AppDabActions
+import Bagbutik
+import Foundation
+import XCTest
+
+final class ListUserInvitationsTests: ActionsTestCase {
+    func testListUserInvitations() async {
+        let response = UserInvitationsResponse(
+            data: [.init(id: "invitation-1", links: .init(self: ""), attributes: .init(email: "sjobs@apple.com", firstName: "Steve", lastName: "Jobs")),
+                   .init(id: "invitation-2", links: .init(self: ""), attributes: .init(email: "forstall@apple.com", firstName: "Scott", lastName: "Forstall"))],
+            links: .init(self: "")
+        )
+        mockBagbutikService.setResponse(response, for: Endpoint(path: "/v1/userInvitations", method: .get))
+        let userInvitations = try! await listUserInvitations()
+        XCTAssertEqual(userInvitations, response.data)
+        XCTAssertEqual(mockLogHandler.logs, [
+            Log(level: .info, message: "🚀 Fetching list of user invitations..."),
+            Log(level: .info, message: "👍 User invitations fetched"),
+            Log(level: .info, message: " ◦ Steve Jobs (sjobs@apple.com)"),
+            Log(level: .info, message: " ◦ Scott Forstall (forstall@apple.com)"),
+        ])
+    }
+}
+
+extension UserInvitation: Equatable {
+    public static func == (lhs: UserInvitation, rhs: UserInvitation) -> Bool {
+        lhs.id == rhs.id
+    }
+}
