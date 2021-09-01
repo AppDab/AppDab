70a2c9a8b0c3519e9ac7972a9eaa06a2e6774123
Morten Bjerg Gregersen <morten@mogee.dk>
2021-09-01 21:44:00 +0200

    No need for platform in registerBundleId

--- Sources/AppDabActions/Actions/Online/BundleIds/RegisterBundleId.swift
+++ Sources/AppDabActions/Actions/Online/BundleIds/RegisterBundleId.swift
@@ -1,8 +1,9 @@
 import Bagbutik
 
 @discardableResult
-public func registerBundleId(identifier: String, name: String, platform: BundleIdPlatform, seedId: String? = nil) async throws -> BundleId {
-    let requestBody = BundleIdCreateRequest(data: .init(attributes: .init(identifier: identifier, name: name, platform: platform, seedId: seedId)))
+public func registerBundleId(identifier: String, name: String, seedId: String? = nil) async throws -> BundleId {
+    // No matter what platform is specified in the request, 'universal' will always be chosen by the API
+    let requestBody = BundleIdCreateRequest(data: .init(attributes: .init(identifier: identifier, name: name, platform: .universal, seedId: seedId)))
     let fullIdentifier = BundleId.fullIdentifier(for: identifier, seedId: seedId)
     ActionsEnvironment.logger.info("🚀 Registering a new bundle ID '\(fullIdentifier)' called '\(name)' for \(platform.prettyName)")
     let bundleIdResponse = try await ActionsEnvironment.service.request(.createBundleId(requestBody: requestBody))
