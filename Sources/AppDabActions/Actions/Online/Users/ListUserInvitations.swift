5904a82c4ffb186cddddd35e54946ef0f4fb294c
Morten Bjerg Gregersen <morten@mogee.dk>
2021-09-03 20:25:51 +0200

    Request all pages when listing (#11)
    * Request all pages when listing
    
    * Update MockBagbutikService
    
    * Create AppDab.xcscheme

--- Sources/AppDabActions/Actions/Online/Users/ListUserInvitations.swift
+++ Sources/AppDabActions/Actions/Online/Users/ListUserInvitations.swift
@@ -3,7 +3,7 @@ import Bagbutik
 @discardableResult
 public func listUserInvitations() async throws -> [UserInvitation] {
     ActionsEnvironment.logger.info("🚀 Fetching list of user invitations...")
-    let response = try await ActionsEnvironment.service.request(.listUserInvitations())
+    let response = try await ActionsEnvironment.service.requestAllPages(.listUserInvitations())
     ActionsEnvironment.logger.info("👍 User invitations fetched")
     response.data.map(\.attributes).forEach { userAttributes in
         ActionsEnvironment.logger.info(" ◦ \(userAttributes!.firstName!) \(userAttributes!.lastName!) (\(userAttributes!.email!))")
