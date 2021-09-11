c2117c1a4ee45dc4673bff83e56b1b90afecab74
Morten Bjerg Gregersen <morten@mogee.dk>
2021-09-12 00:02:08 +0200

    Disable old test

--- Tests/AppDabActionsTests/Actions/Online/Certificates/EnsureCertificateTests.swift
+++ Tests/AppDabActionsTests/Actions/Online/Certificates/EnsureCertificateTests.swift
@@ -2,17 +2,17 @@ import AppDabActions
 import Bagbutik
 import XCTest
 
-final class EnsureCertificateTests: ActionsTestCase {
-    func testEnsureCertificate() async {
-        let fetchResponse = CertificatesResponse(
-            data: [],
-            links: .init(self: ""))
-        mockBagbutikService.setResponse(fetchResponse, for: .init(path: "/v1/certificates", method: .get))
-        try! await ensureCertificate()
-        XCTAssertEqual(mockLogHandler.logs,[
-            Log(level: .info, message: "⏬ Fetching list of available certificates..."),
-            Log(level: .info, message: "🤷🏼 No certificates found online"),
-            Log(level: .error, message: "🚫 In read-only mode, so no new certificate is created"),
-        ])
-    }
-}
+//final class EnsureCertificateTests: ActionsTestCase {
+//    func testEnsureCertificate() async {
+//        let fetchResponse = CertificatesResponse(
+//            data: [],
+//            links: .init(self: ""))
+//        mockBagbutikService.setResponse(fetchResponse, for: .init(path: "/v1/certificates", method: .get))
+//        try! await ensureCertificate()
+//        XCTAssertEqual(mockLogHandler.logs,[
+//            Log(level: .info, message: "⏬ Fetching list of available certificates..."),
+//            Log(level: .info, message: "🤷🏼 No certificates found online"),
+//            Log(level: .error, message: "🚫 In read-only mode, so no new certificate is created"),
+//        ])
+//    }
+//}
