29b1d00188ef52b3c8fef1b8ba93ca7e49f86774
Morten Bjerg Gregersen <morten@mogee.dk>
2022-01-08 23:34:45 +0100

    Add test

---
+++ Tests/AppDabActionsTests/Actions/Online/Apps/UpdateAppPrimaryLocaleTests.swift
@@ -0,0 +1,19 @@
+import AppDabActions
+import Bagbutik
+import XCTest
+
+final class UpdateAppPrimaryLocaleTests: ActionsTestCase {
+    func testUpdateAppPrimaryLocale() async {
+        let updateResponse = AppResponse(
+            data: .init(id: "some-id", links: .init(self: "")),
+            links: .init(self: "")
+        )
+        mockBagbutikService.setResponse(updateResponse, for: Endpoint(path: "/v1/apps/some-id", method: .patch))
+        let app = try! await updateAppPrimaryLocale(forAppId: "some-id", newPrimaryLocale: "da")
+        XCTAssertEqual(app, updateResponse.data)
+        XCTAssertEqual(mockLogHandler.logs, [
+            Log(level: .info, message: "🚀 Updating primary locale 'da' for app with id 'some-id'..."),
+            Log(level: .info, message: "👍 Primary locale updated")
+        ])
+    }
+}
