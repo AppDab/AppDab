523ed139bdc3fba709e3022ea67e3bce47862215
Morten Bjerg Gregersen <morten@mogee.dk>
2022-04-13 23:15:03 +0200

    Add option to skip incrementing build number in info.plists

--- Sources/AppDabActions/Actions/BuildNumber/IncrementBuildNumber.swift
+++ Sources/AppDabActions/Actions/BuildNumber/IncrementBuildNumber.swift
@@ -3,12 +3,16 @@
  Increment the build number for a project
  
  - Parameter xcodeProjPath: The path to a specific Xcode project. If this is not specified, it will look in the current directory.
+ - Parameter includingInfoPlist: Also update the build number in Info.plist files. Newer Xcode projects don't need this as the files are generated build time.
  - Remark: This uses Apple's `agvtool`.
  */
-public func incrementBuildNumber(xcodeProjPath: String? = nil) throws {
+public func incrementBuildNumber(xcodeProjPath: String? = nil, includingInfoPlists: Bool = false) throws {
     let path = getPathContainingXcodeProj(xcodeProjPath)
     ActionsEnvironment.logger.info("✍️ Incrementing build number...")
-    let command = "xcrun agvtool next-version -all"
+    var command = "xcrun agvtool next-version"
+    if includingInfoPlists {
+        command = command + " -all"
+    }
     ActionsEnvironment.logger.trace("⚡️ \(command)")
     let output = try ActionsEnvironment.shell.run(command, at: path)
     ActionsEnvironment.logger.info("📔 Output from agvtool:\n\(output)")
